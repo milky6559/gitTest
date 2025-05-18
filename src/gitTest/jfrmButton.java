c758842ff77a0e8ea92e8ff083fd04c5202cd82f
LAPTOP-HOG67TJK\Milky <milky6559@gmail.com>
2025-05-18 22:31:46 +0800

    Signed-off-by: LAPTOP-HOG67TJK\Milky <milky6559@gmail.com>

---
+++ src/gitTest/jfrmButton.java
@@ -0,0 +1,64 @@
+package gitTest;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class jfrmButton extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					jfrmButton frame = new jfrmButton();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public jfrmButton() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(246, 93, 46, 15);
+		panel.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				lblNewLabel.setText("good");
+			}
+		});
+		btnNewButton.setBounds(61, 70, 87, 23);
+		panel.add(btnNewButton);
+	}
+}
