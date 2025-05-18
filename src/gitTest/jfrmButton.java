1ca9fcebf5fee01e8487235858149325ddad1e89
LAPTOP-HOG67TJK\Milky <milky6559@gmail.com>
2025-05-18 22:40:50 +0800

    Signed-off-by: LAPTOP-HOG67TJK\Milky <milky6559@gmail.com>

--- src/gitTest/jfrmButton.java
+++ src/gitTest/jfrmButton.java
@@ -55,7 +55,7 @@ public class jfrmButton extends JFrame {
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				lblNewLabel.setText("good");
+				lblNewLabel.setText("good1");
 			}
 		});
 		btnNewButton.setBounds(61, 70, 87, 23);
