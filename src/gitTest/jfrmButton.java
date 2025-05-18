bb36395a353c52e8716a38cf33c79d720d1ccbb6
LAPTOP-HOG67TJK\Milky <milky6559@gmail.com>
2025-05-19 01:07:50 +0800

    change

--- src/gitTest/jfrmButton.java
+++ src/gitTest/jfrmButton.java
@@ -55,7 +55,7 @@ public class jfrmButton extends JFrame {
 		JButton btnNewButton = new JButton("New button");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				lblNewLabel.setText("good1");
+				lblNewLabel.setText("goodness");
 			}
 		});
 		btnNewButton.setBounds(61, 70, 87, 23);
